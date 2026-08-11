3593a48768ffd6ea9b8ed07c6b63b830321ba847
Domba Malam <dombamalam@Mac.mshome.net>
2026-08-11 09:05:11 +0700

    Fix blank screen GitHub Pages (.nojekyll & base path)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/web-track/', // <-- WAJIB ADA (sesuai nama repository GitHub Anda)
+  base: '/web-track/', // Nama repository GitHub Anda
+  build: {
+    chunkSizeWarningLimit: 1600, // Menghilangkan peringatan warna kuning >500kB
+  }
 })
