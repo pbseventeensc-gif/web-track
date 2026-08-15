823a9d1e434e8a80fb3376f31ad27b91ef85629a
Domba Malam <dombamalam@Hendri.local>
2026-08-16 00:38:26 +0700

    fix: ubah vite base path ke root agar jalan di vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/web-track/', // Nama repository GitHub Anda
+  base: '/', // Sudah diubah ke root ('/') khusus untuk Vercel
   build: {
     chunkSizeWarningLimit: 1600, // Menghilangkan peringatan warna kuning >500kB
   }
